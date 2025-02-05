ad94d1d9a5801d29e5ed8d37ef100375c75e43c2
erin foley <erindagfoley@gmail.com>
2025-02-05 17:22:37 -0500

    added starter docs

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <div>
+      <h1>My Projects</h1>
+      <p>
+        Projects will go here
+      </p>
+    </div>
+  );
+}
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,10 @@
+export default function Blog() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>
+        Resume will go here
+      </p>
+    </div>
+  );
+}
