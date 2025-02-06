b8d3159b5aaca88aff8a6dc034afdf8fef1f1c48
erin foley <erindagfoley@gmail.com>
2025-02-06 16:08:16 -0500

    added project card looping

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,22 +1,40 @@
 import ProjectCard from "../components/ProjectCard";
-
+import MonsonMeadowsLodge from "../assets/Monson-Meadows-Lodge-thumbnail.png"
 export default function Projects() {
   const projectLoop = [
     {
       title: "VoyageOne Monson Meadows Lodge",
-      image: "./assets/Monson Meadows Lodge thumbnail.png",
+      image: MonsonMeadowsLodge,
       deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
       githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
     },
     {
       title: "VoyageOne Monson Meadows Lodge",
-      image: "./assets/Monson Meadows Lodge thumbnail.png",
+      image: MonsonMeadowsLodge,
       deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
       githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
     },
     {
       title: "VoyageOne Monson Meadows Lodge",
-      image: "./assets/Monson Meadows Lodge thumbnail.png",
+      image: MonsonMeadowsLodge,
+      deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
+      githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
+    },
+    {
+      title: "VoyageOne Monson Meadows Lodge",
+      image: MonsonMeadowsLodge,
+      deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
+      githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
+    },
+    {
+      title: "VoyageOne Monson Meadows Lodge",
+      image: MonsonMeadowsLodge,
+      deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
+      githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
+    },
+    {
+      title: "VoyageOne Monson Meadows Lodge",
+      image: MonsonMeadowsLodge,
       deployedLink: "https://wick000.github.io/Monson-Meadows-Lodge/",
       githubLink: "https://github.com/erindagfoley/Monson-Meadows-Lodge.git"
     },
