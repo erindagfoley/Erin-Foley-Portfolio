304f5f7531aa4777f4ef5a3f9d89e0521fc59e0f
erin foley <erindagfoley@gmail.com>
2025-02-06 18:47:15 -0500

    added font styles and background styles to project

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,6 +2,7 @@ import './ProjectCard.css';
 const ProjectCard = ({ title, image, deployedLink, githubLink }) => {
 
   return (
+    <div className="px-6 pt-4 pb-2 flex justify-center">
     <section className="max-w-sm rounded overflow-hidden shadow-lg">
       <div className="px-6 py-4">
         <h5 className="font-bold text-xl mb-2">{title}</h5>
@@ -9,10 +10,11 @@ const ProjectCard = ({ title, image, deployedLink, githubLink }) => {
       </div>
 
       <div className="px-6 pt-4 pb-2 flex justify-center">
-        <p className=" bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 "> <a href={deployedLink}>Link</a></p>
-        <p className=" bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 "><a href={githubLink}>Link</a></p>
+        <p className=" bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 "> <a href={deployedLink}>Deployed</a></p>
+        <p className=" bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 "><a href={githubLink}>GitHub Repo</a></p>
       </div>
     </section>
+    </div>
   );
 }
 
