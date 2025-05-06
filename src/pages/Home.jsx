17eed5d7fc260c28311798a49ddd048fe29fccec
erin foley <erindagfoley@gmail.com>
2025-05-06 11:54:58 -0400

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,8 +22,14 @@ export default function Home() {
   return (
     <div>
       <h2 style={{ padding: '1rem' }}>About Me</h2>
-      <p style={{ padding: '1rem' }}>
-        I am a passionate career-changer who transitioned from teaching to web development, bringing a unique blend of creativity, problem-solving, and a love for learning to the tech world. My years in education honed my ability to break down complex concepts and communicate effectively, skills that now empower me to craft intuitive, user-friendly web experiences. With a strong foundation in HTML, CSS, JavaScript, and modern frameworks, I thrive on building dynamic and responsive websites that make a meaningful impact. I’m driven by a commitment to continuous growth, collaboration, and leveraging technology to solve real-world problems.
+      <p style={{ paddingLeft: '1rem' }}>
+        I am a passionate career-changer who transitioned from teaching to web development, bringing a unique blend of creativity, problem-solving, and a love for learning to the tech world. My years in education honed my ability to break down complex concepts and communicate effectively, skills that now empower me to craft intuitive, user-friendly web experiences. 
+      </p>
+      <p style={{ paddingLeft: '1rem' }}>
+       With a strong foundation in HTML, CSS, JavaScript, Java, and modern frameworks, I thrive on building dynamic and responsive websites that make a meaningful impact. I’m driven by a commitment to continuous growth, collaboration, and leveraging technology to solve real-world problems.
+      </p>
+      <p style={{ paddingLeft: '1rem' }}>
+        When I'm not developing, I enjoy hiking, skiing, kayaking, biking, swimming, and all things outdoors. I also enjoy spending time with my husband Mike and our four pets, Greta, Eddie, Hank, and Mike the cat.
       </p>
       {/* Render the MyComponent here */}
       <MyComponent />
